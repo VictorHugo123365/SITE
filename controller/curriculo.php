edde41eeba57ccdd666c720bb5443714bcd65fe8
VictorHugo123365 <victor.hugo.silva14@escola.pr.gov.br>
2025-07-29 09:29:06 -0300

    Update curriculo.php

--- controller/curriculo.php
+++ controller/curriculo.php
@@ -1,1 +1,22 @@
+<?php
+$nome = $_POST["nome"];
+$telefone = $_POST[telefone];
+$email = %_POST[email];
+$endereco = $_POST[endereco];
+$efc = $_POST["efc"];
+$emc = $_POST["emc"];
+$esc = $_POST["esc"];
+$pgc = $_POST["pgc"];
+$mc = $_POST["mc"];
 
+$empresa = $_POST["empresa"];
+$data_admissao = $_POST["data_admissao"];
+$data_demissao = $_POST["data_demissao"];
+$atividade_exercida = $_POST["atividade_exercida"];
+
+$descricao = $_POST["descricao"];
+$horas = $_POST["horas"];
+ano_conclusao = $_POST["ano_conclusao"];
+
+$informacoes = $_POST["informacoes"];
+?>
